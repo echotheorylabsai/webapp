4581ba54df9dafde33d99b56d3b9a7baa9d75044
Shubh Shukla <shubham.shukla77@hotmail.com>
2025-09-01 23:10:26 -0400

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import { ThemeProvider } from '../lib/ThemeContext';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider>{children}</ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
